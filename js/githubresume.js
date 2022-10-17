ae049a7561d72d87d6fc118a9a8639a4d7dc662b
Joshua Jones <joshuabrandonjones@gmail.com>
2022-10-16 21:22:23 -0500

    update

--- js/githubresume.js
+++ js/githubresume.js
@@ -15,13 +15,13 @@ var trackerId = 'UA-21222559-1';
 })();
 
 $(document).ready(function() {
-    try {
+    /*try {
         if (urlParams[0] !== undefined) {
             username = urlParams[0];
             run();
-        } else {
+        } else {*/
             home();
-        }
+      //  }
     } catch (err) {
         try {
             console.log(err);
